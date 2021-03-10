319ddec19fcb87af02e593c1d76d4be68053831c
Paolo Tagliaferri <paolotagliaferri@horizenlabs.io>
2021-03-10 15:26:28 +0100

    Improved the error handling; added some unit tests.

--- cctp_primitives/src/bit_vector/compression.rs
+++ cctp_primitives/src/bit_vector/compression.rs
@@ -4,6 +4,8 @@ use std::io::{Read, Write};
 use bzip2::read::{BzEncoder, BzDecoder};
 use flate2::{Compression as GzipCompression, write::GzEncoder, read::GzDecoder};
 
+type Error = Box<dyn std::error::Error>;
+
 pub enum CompressionAlgorithm {
     Uncompressed,
     Bzip2,
@@ -23,74 +25,154 @@ impl TryFrom<u8> for CompressionAlgorithm {
     }
 }
 
-pub fn compress_bit_vector(raw_bit_vector: &[u8], algorithm: CompressionAlgorithm) -> Result<Vec<u8>, &str> {
-    let mut compressed_bit_vector;
+pub fn compress_bit_vector(raw_bit_vector: &[u8], algorithm: CompressionAlgorithm) -> Result<Vec<u8>, Error> {
+    let compressed_bit_vector_result;
 
     match algorithm {
-        CompressionAlgorithm::Uncompressed => compressed_bit_vector = raw_bit_vector.to_vec(),
-        CompressionAlgorithm::Bzip2 => compressed_bit_vector = bzip2_compress(raw_bit_vector),
-        CompressionAlgorithm::Gzip => compressed_bit_vector = gzip_compress(raw_bit_vector),
+        CompressionAlgorithm::Uncompressed => compressed_bit_vector_result = Ok(raw_bit_vector.to_vec()),
+        CompressionAlgorithm::Bzip2 => compressed_bit_vector_result = bzip2_compress(raw_bit_vector),
+        CompressionAlgorithm::Gzip => compressed_bit_vector_result = gzip_compress(raw_bit_vector),
     }
 
-    compressed_bit_vector.insert(0, algorithm as u8);
-    Ok(compressed_bit_vector)
+    if compressed_bit_vector_result.is_ok() {
+        let mut compressed_bit_vector = compressed_bit_vector_result.unwrap();
+        compressed_bit_vector.insert(0, algorithm as u8);
+
+        return Ok(compressed_bit_vector);
+    }
+
+    compressed_bit_vector_result
 }
 
-pub fn decompress_bit_vector(compressed_bit_vector: &[u8], expected_size: usize) -> Result<Vec<u8>, String> {
-    let raw_bit_vector;
+pub fn decompress_bit_vector(compressed_bit_vector: &[u8], expected_size: usize) -> Result<Vec<u8>, Error> {
+    
         
-    match compressed_bit_vector[0].try_into() {
-        Ok(CompressionAlgorithm::Uncompressed) => raw_bit_vector = compressed_bit_vector[1..].to_vec(),
-        Ok(CompressionAlgorithm::Bzip2) => raw_bit_vector = bzip2_decompress(&compressed_bit_vector[1..]),
-        Ok(CompressionAlgorithm::Gzip) => raw_bit_vector = gzip_decompress(&compressed_bit_vector[1..]),
-        Err(_) => return Err(String::from("Compression algorithm not supported"))
+    let raw_bit_vector_result =  match compressed_bit_vector[0].try_into() {
+        Ok(CompressionAlgorithm::Uncompressed) => Ok(compressed_bit_vector[1..].to_vec()),
+        Ok(CompressionAlgorithm::Bzip2) => bzip2_decompress(&compressed_bit_vector[1..]),
+        Ok(CompressionAlgorithm::Gzip) => gzip_decompress(&compressed_bit_vector[1..]),
+        Err(_) => Err("Compression algorithm not supported")?
+    }?;
+
+    if raw_bit_vector_result.len() != expected_size {
+        Err(format!("Wrong bit vector size. Expected {} bytes, found {} bytes", expected_size, raw_bit_vector_result.len()))?
     }
 
-    if raw_bit_vector.len() != expected_size {
-        return Err(format!("Wrong bit vector size. Expected {} bytes, found {} bytes", expected_size, raw_bit_vector.len()));
-    }
-
-    Ok(raw_bit_vector)
+    Ok(raw_bit_vector_result)
 }
 
-fn bzip2_compress(bit_vector: &[u8]) -> Vec<u8> {
+fn bzip2_compress(bit_vector: &[u8]) -> Result<Vec<u8>, Error> {
     let mut compressor = BzEncoder::new(bit_vector, bzip2::Compression::best());
     let mut bzip_compressed = Vec::new();
-    compressor.read_to_end(&mut bzip_compressed).unwrap();
-    bzip_compressed
+    compressor.read_to_end(&mut bzip_compressed)?;
+
+    Ok(bzip_compressed)
 }
 
-fn bzip2_decompress(compressed_bit_vector: &[u8]) -> Vec<u8> {
+fn bzip2_decompress(compressed_bit_vector: &[u8]) -> Result<Vec<u8>, Error> {
     let mut uncompressed_bitvector = Vec::new();
     let mut decompressor = BzDecoder::new(compressed_bit_vector);
-    decompressor.read_to_end(&mut uncompressed_bitvector).unwrap();
-    uncompressed_bitvector
+    decompressor.read_to_end(&mut uncompressed_bitvector)?;
+    
+    Ok(uncompressed_bitvector)
 }
 
-fn gzip_compress(bit_vector: &[u8]) -> Vec<u8> {
+fn gzip_compress(bit_vector: &[u8]) -> Result<Vec<u8>, Error> {
     let mut e = GzEncoder::new(Vec::new(), GzipCompression::best());
-    e.write_all(bit_vector).unwrap();
-    e.finish().unwrap()
+    e.write_all(bit_vector)?;
+    let result = e.finish()?;
+
+    Ok(result)
 }
 
-fn gzip_decompress(compressed_bit_vector: &[u8]) -> Vec<u8> {
+fn gzip_decompress(compressed_bit_vector: &[u8]) -> Result<Vec<u8>, Error> {
     let mut uncompressed_bitvector = Vec::new();
     let mut e = GzDecoder::new(compressed_bit_vector);
-    e.read_to_end(&mut uncompressed_bitvector).unwrap();
-    uncompressed_bitvector
+    e.read_to_end(&mut uncompressed_bitvector)?;
+
+    return Ok(uncompressed_bitvector);
 }
 
 #[cfg(test)]
 mod test {
 
     use super::*;
+    use rand::{Rng, SeedableRng};
 
-    #[test]
-    fn check_compression_algorithm() {
-        let empty_bit_vector: Vec<u8> = Vec::with_capacity(0);
+    fn generate_random_bit_vector(seed: u64) -> Vec<u8> {
+        let mut random_generator = rand_chacha::ChaChaRng::seed_from_u64(seed);
+        let bit_vector_size: u16 = random_generator.gen();
 
-        let compressed_bit_vector = compress_bit_vector(&empty_bit_vector, CompressionAlgorithm::Uncompressed);
-        assert_eq!(compressed_bit_vector.unwrap().len(), 1);
+        let mut bit_vector: Vec<u8> = Vec::with_capacity(bit_vector_size as usize);
+
+        for _ in 0..bit_vector_size {
+            bit_vector.push(random_generator.gen());
+        }
+
+        bit_vector
     }
 
+    #[test]
+    fn empty_bit_vector_compression() {
+        let empty_bit_vector: Vec<u8> = Vec::with_capacity(0);
+
+        let compressed_bit_vector = compress_bit_vector(&empty_bit_vector, CompressionAlgorithm::Uncompressed).unwrap();
+        assert_eq!(compressed_bit_vector.len(), empty_bit_vector.len() + 1);
+        assert_eq!(compressed_bit_vector[0], CompressionAlgorithm::Uncompressed as u8);
+        assert_eq!(decompress_bit_vector(&compressed_bit_vector, empty_bit_vector.len()).unwrap(), empty_bit_vector);
+
+        let compressed_bit_vector = compress_bit_vector(&empty_bit_vector, CompressionAlgorithm::Bzip2).unwrap();
+        assert!(compressed_bit_vector.len() > empty_bit_vector.len());
+        assert_eq!(compressed_bit_vector[0], CompressionAlgorithm::Bzip2 as u8);
+        assert_eq!(decompress_bit_vector(&compressed_bit_vector, empty_bit_vector.len()).unwrap(), empty_bit_vector);
+
+        let compressed_bit_vector = compress_bit_vector(&empty_bit_vector, CompressionAlgorithm::Gzip).unwrap();
+        assert!(compressed_bit_vector.len() > empty_bit_vector.len());
+        assert_eq!(compressed_bit_vector[0], CompressionAlgorithm::Gzip as u8);
+        assert_eq!(decompress_bit_vector(&compressed_bit_vector, empty_bit_vector.len()).unwrap(), empty_bit_vector);
+    }
+
+    #[test]
+    fn expected_bit_vector_size() {
+        let seed: u64 = rand::thread_rng().gen();
+        let original_bit_vector: Vec<u8> = generate_random_bit_vector(seed);
+
+        let mut wrong_bit_vector_size: usize = original_bit_vector.len();
+
+        while wrong_bit_vector_size != original_bit_vector.len() {
+            wrong_bit_vector_size = rand::thread_rng().gen();
+        }
+
+        let compressed_bit_vector = compress_bit_vector(&original_bit_vector, CompressionAlgorithm::Uncompressed).unwrap();
+        assert!(decompress_bit_vector(&compressed_bit_vector, original_bit_vector.len()).is_ok(), "Decompression error using seed={}", seed);
+        assert!(decompress_bit_vector(&compressed_bit_vector, original_bit_vector.len() + 1).is_err(), "Unexpected behavior using seed={}", seed);
+
+        let compressed_bit_vector = compress_bit_vector(&original_bit_vector, CompressionAlgorithm::Bzip2).unwrap();
+        assert!(decompress_bit_vector(&compressed_bit_vector, original_bit_vector.len()).is_ok(), "Bzip2 decompression error using seed={}", seed);
+        assert!(decompress_bit_vector(&compressed_bit_vector, original_bit_vector.len() + 1).is_err(), "Unexpected Bzip2 behavior using seed={}", seed);
+
+        let compressed_bit_vector = compress_bit_vector(&original_bit_vector, CompressionAlgorithm::Gzip).unwrap();
+        assert!(decompress_bit_vector(&compressed_bit_vector, original_bit_vector.len()).is_ok(), "Gzip decompression error using seed={}", seed);
+        assert!(decompress_bit_vector(&compressed_bit_vector, original_bit_vector.len() + 1).is_err(), "Unexpected Gzip behavior using seed={}", seed);
+    }
+
+    #[test]
+    fn wrong_bit_vector_compression_format() {
+        let seed: u64 = rand::thread_rng().gen();
+        let original_bit_vector: Vec<u8> = generate_random_bit_vector(seed);
+
+        let mut compressed_bit_vector = compress_bit_vector(&original_bit_vector, CompressionAlgorithm::Uncompressed).unwrap();
+        compressed_bit_vector[0] = CompressionAlgorithm::Bzip2 as u8;
+        assert!(decompress_bit_vector(&compressed_bit_vector, original_bit_vector.len()).is_err());
+        compressed_bit_vector[0] = CompressionAlgorithm::Gzip as u8;
+        assert!(decompress_bit_vector(&compressed_bit_vector, original_bit_vector.len()).is_err());
+
+        compressed_bit_vector = compress_bit_vector(&original_bit_vector, CompressionAlgorithm::Bzip2).unwrap();
+        compressed_bit_vector[0] = CompressionAlgorithm::Gzip as u8;
+        assert!(decompress_bit_vector(&compressed_bit_vector, original_bit_vector.len()).is_err());
+
+        compressed_bit_vector = compress_bit_vector(&original_bit_vector, CompressionAlgorithm::Gzip).unwrap();
+        compressed_bit_vector[0] = CompressionAlgorithm::Bzip2 as u8;
+        assert!(decompress_bit_vector(&compressed_bit_vector, original_bit_vector.len()).is_err());
+    }
 }
