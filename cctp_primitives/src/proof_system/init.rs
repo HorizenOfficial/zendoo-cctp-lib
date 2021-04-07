87aa3c7b354c2a66214d735f4ed26204ea7b0e72
Phoinic <max@phoinic.com.ua>
2021-04-07 09:48:39 +0300

    Lazy static caching introduced

--- cctp_primitives/src/proof_system/init.rs
+++ cctp_primitives/src/proof_system/init.rs
@@ -3,8 +3,10 @@ use super::type_mapping::*;
 use algebra::curves::tweedle::dee::Affine;
 use algebra::{FromBytes, ToBytes};
 
+use lazy_static::lazy_static;
+
 use std::{
-    sync::Arc,
+    sync::Mutex,
     fs::File,
     path::Path,
     io::{
@@ -14,7 +16,18 @@ use std::{
     }
 };
 
-pub fn load_generators(num_generators: usize, file_path: &str) -> IoResult<Arc<Vec<Affine>>> {
+lazy_static! {
+    static ref CACHED_GENERATORS: Mutex<Vec<Affine>> = Mutex::new(vec![]);
+}
+
+pub fn load_generators(num_generators: usize, file_path: &str) -> IoResult<Vec<Affine>> {
+
+    let mut cache = CACHED_GENERATORS.lock().unwrap();
+
+    if cache.len() == num_generators {
+        return Ok((*cache).clone());
+    }
+
     let generators;
     if Path::new(file_path).exists() {
         let fs = File::open(file_path)?;
@@ -29,7 +42,10 @@ pub fn load_generators(num_generators: usize, file_path: &str) -> IoResult<Arc<V
         (generators.len() as u32).write(&fs)?;
         generators.write(&fs)?;
     }
-    Ok(Arc::new(generators))
+
+    cache.clone_from(&generators);
+
+    Ok((*cache).clone())
 }
 
 #[cfg(test)]
