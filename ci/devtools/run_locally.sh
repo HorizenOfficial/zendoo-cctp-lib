48823c6a5f20924f9e51466b74cde7413b5cd12a
cronicc <cronic@zensystem.io>
2021-05-20 14:56:28 +0000

    Don't use bash builtin 'jobs' for array name

--- ci/devtools/run_locally.sh
+++ ci/devtools/run_locally.sh
@@ -9,9 +9,9 @@ exec -c $0
 
 workdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." &> /dev/null && pwd )"
 mapfile -t globals < <(docker run --rm -v "$workdir":/workdir mikefarah/yq e '.env.global.[]' .travis.yml)
-mapfile -t jobs < <(docker run --rm -v "$workdir":/workdir mikefarah/yq e '.jobs.include.[].env' .travis.yml)
+mapfile -t runners < <(docker run --rm -v "$workdir":/workdir mikefarah/yq e '.jobs.include.[].env' .travis.yml)
 
-for i in "${!jobs[@]}"; do
-  ( source <(for var in "${globals[@]}"; do echo "export $var"; done; for var in "${jobs[$i]}"; do echo "export $var"; done); \
+for i in "${!runners[@]}"; do
+  ( source <(for var in "${globals[@]}"; do echo "export $var"; done; for var in "${runners[$i]}"; do echo "export $var"; done); \
   "$workdir"/ci/script.sh 2>&1 | tee -a "$workdir/ci/devtools/job-$i.log" )
 done
